f8e057f907ea295247ae3cb79aca7cd6b997622a
Wetwlly <xmikecampx@aol.com>
2011-11-30 07:04:38 -0800

    small comment out

--- Form1.cs
+++ Form1.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Diagnostics;
+//using System.Diagnostics; //used with opening files, used for testing
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
